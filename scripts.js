f7e970456e68cb7d4258c90dc0cd203f405a38ac
EHoldener <eholdener12@gmail.com>
2022-06-21 17:26:06 -0700

    got clicking for moves workign finally

--- scripts.js
+++ scripts.js
@@ -5,12 +5,17 @@ const Gameboard = (() => {
   const init = function () {
     row1 = document.createElement('div');
     row1.classList.add('row');
+    row1.classList.add('justify-content-center');
+    row1.classList.add('pb-3');
     row1.setAttribute('id', 'row1');
     row2 = document.createElement('div');
     row2.classList.add('row');
+    row2.classList.add('justify-content-center');
+    row2.classList.add('pb-3');
     row2.setAttribute('id', 'row2');
     row3 = document.createElement('div');
     row3.classList.add('row');
+    row3.classList.add('justify-content-center');
     row3.setAttribute('id', 'row3');
     for (let i = 0; i < 9; i++) {
       tile = creation(0, i, '');
@@ -30,7 +35,7 @@ const Gameboard = (() => {
 
   const reset = function () {
     gameboard.forEach(cell => {
-      cell.dataset.player = '';
+      cell.dataset.player = 0;
       cell.querySelector('img').src = '';
     })
   };
@@ -38,23 +43,21 @@ const Gameboard = (() => {
   const creation = function (player, index, image) {
     column = document.createElement('div');
     column.classList.add('col');
+    column.classList.add('flex-grow-0');
+    column.classList.add('ttt-cell');
     column.dataset.id = index;
     column.dataset.player = player;
 
-    card = document.createElement('div');
-    card.classList.add('card');
-
     context = document.createElement('button');
-    context.classList.add('card-text');
     context.classList.add('btn');
-
+    context.classList.add('box-styling');
+    context.disabled = true;
     
     img = document.createElement('img');
     img.src = image;
 
     context.appendChild(img);
-    card.appendChild(context);
-    column.appendChild(card);
+    column.appendChild(context);
     return column;
   };
 
@@ -73,39 +76,68 @@ const Player = (() => {
 
 function playerFactory(name, icon, winMessage) {
   const play = function (e) {
-    const img = e.target.querySelector('img');
-    if (img.src.length === 0) {
+    console.log('play');
+    console.log(e.target);
+    console.log(e.target.dataset.player)
+    if(e.target.dataset.player == 0) {
+      console.log('if statement');
+      const img = e.target.querySelector('img');
       img.src = icon;
       return true;
-    };
+    } else {
+      return false;
+    }
   };
 
   return { play, name, icon, winMessage };
 }
 
 const Game = (() => {
-  console.log('game')
   let turn = Player.FIRST;
-  const totalRounds = 8;
+  const totalRounds = 9;
   let round = 0;
   let player1 = playerFactory('player 1', 'assets/x.png', 'X wins');
-  let player2 = playerFactory('player 2', 'assets/y.png', 'Y wins');
+  let player2 = playerFactory('player 2', 'assets/o.png', 'Y wins');
   Gameboard.init();
   let gameboard = Gameboard.gameboard;
-  document.getElementById('new-game-button').removeEventListener('click', function() {
-    Game()
-  });
-  document.getElementById('new-game-button').addEventListener('click', function() {
-    reset();
-  })
 
-  function play() {
+  document.getElementById('new-game-button').remove();
+
+  newGameButton = document.createElement('btn');
+  newGameButton.classList.add('btn');
+  newGameButton.classList.add('btn-danger');
+  newGameButton.innerHTML = 'New Game';
+  newGameButton.id = 'reset-game-button';
+
+  newGameButton.addEventListener('click', function() {
+    reset();
+  });
+
+  document.getElementById('game-button').appendChild(newGameButton);
+
+  function play(e) {
     if (round != totalRounds) {
       switch(turn) {
-        case FIRST:
-          player1.play();
-        case SECOND:
-          player2.play();
+        case Player.FIRST:
+          if (player1.play(e)) {
+            turn = Player.SECOND;
+            e.target.dataset.player = Player.FIRST;
+            round++;
+            if(checkWinner) {
+              // player 1 wins
+            }
+          }
+          break;
+        case Player.SECOND:
+          if (player2.play(e)) {
+            turn = Player.FIRST;
+            e.target.dataset.player = Player.SECOND;
+            round++;
+            if(checkWinner) {
+              // player 2 wins
+            }
+          }
+
       };
     } else {
       // display tie message
@@ -113,9 +145,9 @@ const Game = (() => {
   };
 
   function reset() {
-
+    turn = Player.FIRST;
     round = 0;
-    gameboard.reset();
+    Gameboard.reset();
   };
 
   function checkWinner() {
@@ -125,10 +157,8 @@ const Game = (() => {
   gameboard.forEach(cell => {
     cell.addEventListener('click', play);
   });
-
-  return { play };
 });
 
 document.getElementById('new-game-button').addEventListener('click', function() {
   Game()
-})
+});
